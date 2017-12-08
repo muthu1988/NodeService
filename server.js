ccccf67ae8cfb142e0fdc925c19c6972cd9c33a0
Muthu Kumaran <muthukumaran.a@nationstarmail.com>
2017-12-09 01:23:00 +0530

    Enable CORS

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/data', function (req, res) {
     res.json({ data: 'Data From Node Service' })
 });
@@ -9,6 +15,6 @@ app.get('/', function (req, res) {
     res.send('****Node App Built with Express Js****')
 });
 
-app.listen(3000);
+app.listen(4000);
 
-console.log('Node Service Running On Port: 3000');
+console.log('Node Service Running On Port: 4000');
